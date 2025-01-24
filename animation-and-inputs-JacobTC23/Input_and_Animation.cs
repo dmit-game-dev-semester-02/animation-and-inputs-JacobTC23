d4be95ce7533d6fab3575871f0b3f0919ab198b7
JacobTC23 <jacobcooper472@gmail.com>
2025-01-24 15:38:04 -0700

    commit

--- animation-and-inputs-JacobTC23/Input_and_Animation.cs
+++ animation-and-inputs-JacobTC23/Input_and_Animation.cs
@@ -11,7 +11,7 @@ public class Input_and_Animation : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     private Texture2D _background, _balloon;
-    private CelAnimationSequence _flag, _sequence02;
+    private CelAnimationSequence _flag, _bird;
     private CelAnimationPlayer _animation1, _Animation2;
     private float _BalloonX;
     private float _BalloonY;
@@ -38,11 +38,20 @@ public class Input_and_Animation : Game
         _background = Content.Load<Texture2D>("Houses");
         _balloon = Content.Load<Texture2D>("Balloon");
 
-        Texture2D spriteSheet = Content.Load<Texture2D>("flag_red");
-         _flag = new CelAnimationSequence(spriteSheet, 126, 1 / 4f);
+        Texture2D spriteSheet = Content.Load<Texture2D>("flag_animation");
+        // the flag still shakes in place but thats just because it was not made the best I assume.
+         _flag = new CelAnimationSequence(spriteSheet, 60, 1 / 4f);
          _animation1 = new CelAnimationPlayer();
          _animation1.Play(_flag);
-        
+
+         Texture2D spriteSheet2 = Content.Load<Texture2D>("blue_bird");
+         _bird = new CelAnimationSequence(spriteSheet2,
+            frameWidth: 32,      // Width of each frame
+            frameHeight: 32,     // Height of each frame
+            totalFrames: 24      // Total number of frames
+        );
+         _Animation2 = new CelAnimationPlayer(_bird);
+         _Animation2.Play(_bird);
 
         // TODO: use this.Content to load your game content here
     }
@@ -50,6 +59,7 @@ public class Input_and_Animation : Game
     protected override void Update(GameTime gameTime)
     {
         _animation1.Update(gameTime);
+        _Animation2.Update(gameTime);
         // TODO: Add your update logic here
 
         base.Update(gameTime);
@@ -62,7 +72,8 @@ public class Input_and_Animation : Game
         _spriteBatch.Begin();
         _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
         _spriteBatch.Draw(_balloon, new Vector2(_BalloonX, _BalloonY), Color.White);
-        _animation1.Draw(_spriteBatch, new Vector2(500, 500), SpriteEffects.None);
+        _animation1.Draw(_spriteBatch, new Vector2(500, 550), SpriteEffects.None);
+        _Animation2.Draw(_spriteBatch, new Vector2(100, 100), SpriteEffects.None);
         
         _spriteBatch.End();
 
